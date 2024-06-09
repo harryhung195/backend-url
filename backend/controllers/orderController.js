c5a8a7c1be0ff90caebf727c6163b537eba76c6d
harryhung195 <162422942+harryhung195@users.noreply.github.com>
2024-06-09 19:11:17 +1000

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,8 +6,8 @@ import Stripe from "stripe"
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing user order for frontend
 const placeOrder = async (req,res)=> {
-const frontend_url = "http://localhost:5173";
-    const admin_url = "http://localhost:5174";
+const frontend_url = "http://localhost:5174";
+    
     
 
 try {
